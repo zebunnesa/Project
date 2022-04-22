0d60cda820eb0a433f73a5ae76286e0fa74d412e
zebunnesa <98897821+zebunnesa@users.noreply.github.com>
2022-04-22 17:22:54 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,79 +1,30 @@
 const express=require('express')
 const app=express()
-const morgan= require('morgan')
 
-app.set('view engine','ejs')
-
-
-app.listen(3000)
-
-app.use(express.static('public'))
-
-app.use(morgan('dev'))
+app.set('view engine','ejs');
+app.set('view ','myview');
 
 
-app.use ((req,res)=>{
-    console.log('new request made')
-    console.log(' host', req.hostname)
-    console.log('path',req.path)
-    console.log('method',  req.method)
-    next()
+app.listen(3000);
+app.get('/',(req,res)=>{
+    // res.send('<p>home pages</p>')
+    res.render('index',{title:'Home'})
+    // res.sendFile('./view/index.html',{root: __dirname})
 })
-
-app.use ((req,res,next)=>{
-    console.log('in the next')
-    // console.log('new request made')
-    // console.log(' host', req.hostname)
-    // console.log('path',req.path)
-    // console.log('method',  req.method)
-    next()
-})
-
-
-
-app.get('/',(res,req)=>{
-    //res.render('index', {title: 'home'})
-    blogs=[
-    {title: 'yoshi', snippet: 'lorem ipsum'},
-    {title: 'mario', snippet: 'lorem ipsum'},
-    {title: 'how to defeal browser', snippet: 'lorem ipsum'},
-   ]
-})
-
-app.get('./', (req,res)=>{
-    res.sendFile('./view/index.html',{root:__dirname})
-})
-
 app.get('/about',(req,res)=>{
-    //res.send('<p>about page</p>')
-    res.sendFile('./view/about.html',{root:__dirname})
+    // res.send('<p>about pages</p>')
+    // res.sendFile('./view/about.html',{root: __dirname})
+    res.render('about')
+})
+app.get('/blogs/create',(req,res)=>{
+    res.render()
 })
 
-app.get('./blogs/create' ,(req,res)=>{
-    //res.send('<p>about page</p>')
-   res.render('create' ,{title: 'create a new blog'})
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about')
 })
 
-
-// app.get('/about-us',(req,res)=>{
-//     //res.send('<p>about page</p>')
-//     //res.sendFile('./view/about.html',{root:__dirname})
-
-//     res.redirect('/about')
-// })
-
-// app.use((req,res)=>[
-//     res.sendFile('./view/404.html', {root:__dirname})
-// ])
-
-
-app.get('/about',(req,res)=>{
-    //res.send('<p>about page</p>')
-    res.sendFile('./view/about.html',{root:__dirname})
+app.use((res,req)=>{
+    // res.status(404).sendFile('./view/404.html',{root: __dirname})
+    res.statusCode(404).render('404')
 })
-
-
-
-app.use((req,res)=>[
-    res.status(404).sendFile('./view/404.html', {root:__dirname})
-])
